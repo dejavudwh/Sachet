1c50cb3cfe93aec20b79184a10d2ce2396d616f0
dejavudwh <daweihucs@outlook.com>
2021-09-07 10:36:53 +0800

    complete memory subsystem

--- src/cgroups/subsystems/memory.go
+++ src/cgroups/subsystems/memory.go
@@ -1,14 +1,16 @@
 /*
  * @Author: dejavudwh
  * @Date: 2021-09-07 00:04:08
- * @LastEditTime: 2021-09-07 00:51:33
+ * @LastEditTime: 2021-09-07 10:30:02
  */
 package subsystems
 
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"path"
+	"strconv"
 )
 
 type MemorySubSystem struct {
@@ -16,7 +18,7 @@ type MemorySubSystem struct {
 
 /**
  * @description: Set the memory resource limit of the cgroup
- * @param {string} cgroupPath: cgroup (virtual file system)
+ * @param {string} cgroupPath: cgroup path (virtual file system)
  * @param {*ResourceConfig} res: restriction information
  * @return {*}
  */
@@ -35,6 +37,42 @@ func (s *MemorySubSystem) Set(cgroupPath string, res *ResourceConfig) error {
 	}
 }
 
+/**
+ * @description: Delete the cgroup corresponding to cgroupPath
+ * @param {string} cgroupPath: cgroup path (virtual file system)
+ * @return {*}
+ */
+func (s *MemorySubSystem) Remove(cgroupPath string) error {
+	if subsysCgroupPath, err := GetCgroupPath(s.Name(), cgroupPath, false); err == nil {
+		return os.Remove(subsysCgroupPath)
+	} else {
+		return err
+	}
+}
+
+/**
+ * @description: add process to cgroup corresponding to cgroupPath
+ * @param {string} cgroupPath: cgroup path (virtual file system)
+ * @param {int} pid: process id
+ * @return {*}
+ */
+func (s *MemorySubSystem) Apply(cgroupPath string, pid int) error {
+	if subsysCgroupPath, err := GetCgroupPath(s.Name(), cgroupPath, false); err == nil {
+		if err := ioutil.WriteFile(path.Join(subsysCgroupPath, "tasks"),
+			[]byte(strconv.Itoa(pid)), 0644); err != nil {
+			return fmt.Errorf("set cgroup proc fail %v", err)
+		}
+		return nil
+	} else {
+		return fmt.Errorf("get cgroup %s error: %v", cgroupPath, err)
+	}
+}
+
+/**
+ * @description: Get subsystem name
+ * @param {*}
+ * @return {*}
+ */
 func (s *MemorySubSystem) Name() string {
 	return "memory"
 }
