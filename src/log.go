e4d058ccaeac3088a77751d7bf2a122ba9d68475
dejavudwh <daweihucs@outlook.com>
2021-09-21 19:14:31 +0800

    add log command for container

--- src/log.go
+++ src/log.go
@@ -1,5 +1,33 @@
 /*
  * @Author: dejavudwh
  * @Date: 2021-09-21 17:13:19
- * @LastEditTime: 2021-09-21 19:10:57
+ * @LastEditTime: 2021-09-21 19:13:42
  */
+package main
+
+import (
+	"Scachet/src/container"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	log "github.com/Sirupsen/logrus"
+)
+
+func logContainer(containerName string) {
+	// find dir
+	dirURL := fmt.Sprintf(container.DefaultInfoLocation, containerName)
+	logFileLocation := dirURL + container.ContainerLogFile
+	file, err := os.Open(logFileLocation)
+	defer file.Close()
+	if err != nil {
+		log.Errorf("Log container open file %s error %v", logFileLocation, err)
+		return
+	}
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Errorf("Log container read file %s error %v", logFileLocation, err)
+		return
+	}
+	fmt.Fprint(os.Stdout, string(content))
+}
